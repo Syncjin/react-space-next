c7f73d156b733206c9ead29df7411f6c5f97cae4
leewoojin <dldndldms@gmail.com>
2019-06-05 17:08:16 +0900

    webpack css, scss 사용

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-// Sass import 사용
+// Css, Sass import 사용
 const withSass = require('@zeit/next-sass')
+const withCss = require('@zeit/next-css')
 
 require('dotenv').config()
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 
-module.exports = withSass({
-  cssModules: true,
+module.exports = withSass(withCss({
+  target: 'serverless',
+  
   webpack: config => {
+
     config.plugins = config.plugins || []
 
     config.plugins = [
@@ -19,7 +22,40 @@ module.exports = withSass({
         systemvars: true
       })
     ]
-
+    // config.module.rules.push(
+    //   {
+    //     test: /\.css$/,
+    //     use: [
+    //       'style-loader',
+    //       {
+    //         loader: 'css-loader',
+    //         options: {
+    //           modules: true,
+    //           importLoaders: 1,
+    //           localIdentName:"[name]_[local]_[hash:base64]",
+    //           sourceMap: true,
+    //         }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     test: /\.scss$/,
+    //     use: [
+    //       'style-loader',
+    //       {
+    //         loader: 'css-loader',
+    //         options: {
+    //           modules: true,
+    //           importLoaders: 1,
+    //           localIdentName:"[name]_[local]_[hash:base64]",
+    //           sourceMap: true,
+    //         }
+    //       },
+    //       'sass-loader'
+    //     ]
+    //    }
+    // )
+   
     return config
   }
-})
+}))
